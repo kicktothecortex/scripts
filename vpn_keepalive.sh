057810bf49459ac913023f3df9a8a5ce1619c7b1
digix@protato <digix84@gmail.com>
2014-12-14 13:20:34 -0600

    Adding updates to vpn keepalive

--- vpn_keepalive.sh
+++ vpn_keepalive.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+#VPN_UUID="043164c9-05bf-42ad-83f4-bc368d246326"		# East
+VPN_UUID="16b49df3-c1d5-4c82-9d2d-b9f9398ded33"		# Midwest
+
 while [ "true" ]
 do
 	VPNCON=$(nmcli c status)
 	if [[ $VPNCON != *PIA* ]]; then
 		#echo "Disconnected, trying to reconnect..."
-		(sleep 1s && nmcli c up uuid 16b49df3-c1d5-4c82-9d2d-b9f9398ded33)
+		(sleep 1s && nmcli c up uuid $VPN_UUID)
 	#else
 		#echo "Already connected !"
 	fi
